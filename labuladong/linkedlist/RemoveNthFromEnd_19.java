2ad1b0e0d4e0be74abcee8b4c2899d98679c1fc7
LanceMai <mailiangshi@gmail.com>
2026-01-06 22:37:01 +0800

    refactor(linkedlist): 优化删除链表倒数第N个节点的实现
    - 移除了不必要的cur2指针声明和操作
    - 简化了节点删除逻辑，直接使用前置节点进行操作
    - 删除了手动垃圾回收的代码
    - 添加了

--- labuladong/linkedlist/RemoveNthFromEnd_19.java
+++ labuladong/linkedlist/RemoveNthFromEnd_19.java
@@ -3,6 +3,7 @@ package labuladong.linkedlist;
 /**
  * 删除链表的倒数第N个节点
  * 1、指针先跑n步，双指针。用时22分钟。注意需要使用到目标指针的前置指针，才能操作链表节点remove
+ * 其实，直接找前置节点就行了，不用找
  */
 public class RemoveNthFromEnd_19 {
     public ListNode removeNthFromEnd(ListNode head, int n) {
@@ -13,14 +14,15 @@ public class RemoveNthFromEnd_19 {
         for (int i = 0; i < n; i++) {
             cur1 = cur1.next;
         }
-        ListNode cur2 = head;
+        // ListNode cur2 = head;
         while (cur1 != null) {
             cur1 = cur1.next;
-            cur2 = cur2.next;
+            // cur2 = cur2.next;
             preCur1 = preCur1.next;
         }
-        preCur1.next = cur2.next;
-        cur2 = null; // 手动断开cur2，垃圾回收
+        // preCur1.next = cur2.next;
+        preCur1.next = preCur1.next.next;
+        // cur2 = null; // 手动断开cur2，垃圾回收
         return dummyHead.next;
     }
 
