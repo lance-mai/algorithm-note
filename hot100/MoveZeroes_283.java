72cdcff45b1f6480acd72be3aadfd6ad38553528
LanceMai <mailiangshi@gmail.com>
2026-01-30 22:20:17 +0800

    feat(hot100): 添加移动零算法实现
    - 使用快慢指针方法实现移动零功能
    - 将非零元素前移并保持原有顺序
    - 在数组末尾填充零元素
    - 时间复杂度为O

---
+++ hot100/MoveZeroes_283.java
@@ -0,0 +1,22 @@
+package hot100;
+
+/**
+ * 移动零
+ * 解法1：快慢指针
+ */
+public class MoveZeroes_283 {
+    public void moveZeroes(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != 0) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        while (slow < nums.length) {
+            nums[slow++] = 0;
+        }
+    }
+}
