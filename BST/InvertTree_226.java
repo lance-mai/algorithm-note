5cd646bf760420e67d259cf269615ce8a87fa514
LanceMai <mailiangshi@gmail.com>
2026-02-01 19:59:32 +0800

    feat(BST): 添加翻转二叉树功能实现
    - 实现了两种解法：分解子问题方法和递归遍历方法
    - 添加了TreeNode数据结构定义
    - 实现了解法1：通过递归分解子问题翻转二叉树
    - 实现了解法2：通过前序遍历方式翻转二叉树
    - 提供了完整的二叉树节点构造函数

---
+++ BST/InvertTree_226.java
@@ -0,0 +1,38 @@
+package BST;
+
+/**
+ * 翻转二叉树
+ * 解法1：分解子问题。定义一个函数，有返回值。函数定义为：翻转一个二叉树，返回该二叉树的根节点
+ * 解法2：递归遍历。前序遍历，遍历到该节点的时候，就翻转左右子树
+ */
+public class InvertTree_226 {
+    // 解法2
+    public TreeNode invertTree(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+
+    private void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        traverse(root.left);
+        traverse(root.right);
+    }
+
+    // 解法1
+    public TreeNode invertTree1(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree1(root.left);
+        TreeNode right = invertTree1(root.right);
+        // 翻转
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
